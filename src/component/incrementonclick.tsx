923ea3c5992f236e60839f8ea9b313c3980654a0
Anubhavced <anubhavsrivastava@cedcommerce.com>
2022-01-04 15:54:33 +0530

    document to document1

--- src/component/incrementonclick.tsx
+++ src/component/incrementonclick.tsx
@@ -1,13 +1,43 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 function IncrementOnclick() {
   const [count, setCounter] = useState<number>(0);
+  const [active,setActive] = useState<boolean>(false)
+  const [combine,setCombine] = useState<string | number>()
+
+  useEffect(()=>{
+    let interval:number ;
+        if (active) {
+             interval = window.setInterval(() => {
+                setCounter(count => count + 1);
+            }, 1000);
+          } else if (!active && count !== 0) {
+            setCounter(count);
+          }
+        return () => clearInterval(interval);
+      
+  },[active,count])
 
   const increment = () => {
     setCounter(count +1);
   };
   const decrement = () => {
+      if(count>0 ){
     setCounter(count -1); 
+      }
+  }
+  const timer = () =>{
+    setActive(true)  ; 
+  }
+  const Pausetimer = () => {
+    setActive(!active)  ; 
+    setCounter(count) 
+    setCombine(5)
+  }
+  const  Stoptimer = () =>{
+    setCounter(0) ;
+    setActive(false)
+    setCombine("timer stop")
   }
 
   return (
@@ -15,9 +45,13 @@ function IncrementOnclick() {
       <h2>Counter</h2>
 
       <div>{count}</div>
-
+        <div>{combine}</div>
       <button onClick={increment}>Incr +</button>
       <button onClick={decrement}>Decr -</button>
+      {active?<button onClick={Pausetimer}>Pause timer</button>:<button onClick={timer}>Start timer</button>}
+      
+      {active?<button onClick={Stoptimer}>Stop timer</button>:null}
+      
     </div>
   );
 }
